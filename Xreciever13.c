88a38fe65074510ae5e69f63d12a1ba34d020c14
Unknown <artem-avr@yandex.ru>
2017-07-01 16:01:19 +0300

    Code and readme cleanup
    Wrote proper readme page, cleaned and commented source code

--- Xreciever13.c
+++ Xreciever13.c
@@ -2,8 +2,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-//pb1 - sync and interruption
+//pb1 - sync button
 //pb0 - clock
 //pb2 - data
 
@@ -17,85 +16,83 @@
 #define SYNC_PIN PINB
 #define SYNC_DDR DDRB
 
-char prev;
-int i;
 
-void sendToRFModule (unsigned char recieved)
+void sendToRFModule (unsigned char received)
 {
-    prev = 0xFF;
-    DATA_PORT &= ~(1<<DATA); //установка 0
-    for(i = 0; i < 11; i++)
+    char prev = 0xFF; //don't remember why I used char for this, but it works
+    DATA_PORT &= ~(1<<DATA); //set data line 0 to begin data transmission
+    for(int i = 0; i < 11; i++) //first 2 bytes of sending data are always 0, the rest 8 are the actual command
     {
         while (prev == (CLOCK_PIN & (1<<CLOCK))){} //detects change in clock 
         prev = CLOCK_PIN & (1<<CLOCK); // should be after downward edge of clock, so send bit of data now 
         if (i < 2)
 		{
-            DATA_PORT &= ~(1<<DATA); //установка 0
+            DATA_PORT &= ~(1<<DATA); //set 0
 		}
 		else
 		{
-            if (recieved >> 7)
+            if (received >> 7) //get highest bit of data
 			{
-			    DATA_PORT |= 1<<DATA; //установка 1
+			    DATA_PORT |= 1<<DATA; //set 1
 			}
 			else
 			{
-                DATA_PORT &= ~(1<<DATA); //установка 0
+                DATA_PORT &= ~(1<<DATA); //set 0
 			}
-	    	recieved = recieved << 1;
+	    	received = received << 1; shift data 1 bit left so we can get next bit during next iteration
 		}
 
         while (prev == (CLOCK_PIN & (1<<CLOCK))){} //detects upward edge of clock 
         prev = CLOCK_PIN & (1<<CLOCK);
     }
-    DATA_PORT |= 1<<DATA; //установка 1
+    DATA_PORT |= 1<<DATA; //set 1
 }
 
 int main(void)
 {
 
-    DATA_DDR |= 1<<DATA;   //gpio init
-    CLOCK_DDR &= ~(1<<CLOCK); //gpio init
-	SYNC_DDR &= ~(1<<SYNC); //gpio init
+    DATA_DDR |= 1<<DATA; //configure as output
+    CLOCK_DDR &= ~(1<<CLOCK); //configure as input
+	SYNC_DDR &= ~(1<<SYNC); //configure as input
 
 
-    DATA_PORT |= 1<<DATA; //установка 1
+    DATA_PORT |= 1<<DATA; //set 1
 
-    _delay_ms(3500);
+    _delay_ms(3500); //RF module does some internal initialization after power up, it's good to give it some time for this
 
-	sendToRFModule(0x84);
+	sendToRFModule(0x84); //inits module and turns on central led
 
 	int timer;
 	int flag;
 
 	while(1)
 	{
-        if ((SYNC_PIN & (1<<SYNC)) == 0)
+        if ((SYNC_PIN & (1<<SYNC)) == 0) //check if button is pressed
 		{
 		    flag = 1;
 
             for (timer = 0; (timer < 400) && (flag); timer++)
 			{
-			   if (SYNC_PIN & (1<<SYNC))
+			   if (SYNC_PIN & (1<<SYNC)) //if button has been held less than ~800ms
 			   {
-                   sendToRFModule(0x04);
+                   sendToRFModule(0x04); //send command to start syncronization
 				   flag = 0;
 			   }
                _delay_ms(2);
 			}
-			if (flag)
+			if (flag) //else if button has been held more than ~800ms
 			{
                 sendToRFModule(0xA9);
 				_delay_ms(4);
-				sendToRFModule(0xB6);
+				sendToRFModule(0xB6); //turn on green-red ring of light
 				_delay_ms(4);
-				sendToRFModule(0x09);
+				sendToRFModule(0x09); //send command to disable all controllers
 				_delay_ms(500);
                 sendToRFModule(0xA0);
 				_delay_ms(4);
-				sendToRFModule(0xB0);
+				sendToRFModule(0xB0); //turn off ring of light
 			}
-			_delay_ms(2000);
+			_delay_ms(2000); //and wait ~2s to exclude false actions
 		}
 	}
 }
